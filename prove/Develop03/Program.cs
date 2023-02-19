81122a9907a52675bc1e6c0ae86f6fc928220ba3
alexander98e <echave98alexander@gmail.com>
2023-02-18 21:16:05 -0700

    codeVERS

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -1,9 +1,253 @@
 using System;
+using System.Collections.Generic;
 
-class Program
+namespace ListPrinter
 {
-    static void Main(string[] args)
+    class Program
     {
-        Console.WriteLine("Hello Develop03 World!");
+        static void Main(string[] args)
+        {
+          
+            var months = new List<string>();
+            months.Add("For");
+            months.Add("God");
+            months.Add("so");
+            months.Add("loved");
+            months.Add("the");
+            months.Add("world");
+            months.Add("that");
+            months.Add("he");
+            months.Add("gave");
+            months.Add("his");
+            months.Add("one");
+            months.Add("and");
+            months.Add("only");
+            months.Add("Son,");
+            months.Add("that");
+            months.Add("whoever");
+            months.Add("believes");
+            months.Add("in");
+            months.Add("him");
+            months.Add("shall");
+            months.Add("not");
+            months.Add("perish");
+            months.Add("but");
+            months.Add("have");
+            months.Add("eternal");
+            months.Add("life.");
+            
+
+
+            //using strings.join
+            Console.WriteLine(string.Join(" ", months));
+            
+
+            Console.WriteLine("Memorize your Verse!!!...Continue...?");
+            
+            string userName = Console.ReadLine();
+            
+            {
+                months.RemoveAt(2);
+                months.Insert(2,"___");
+                Console.WriteLine(string.Join(" ", months));
+                Console.WriteLine("Memorize your Verse!!!...Continue...?");
+                string userlast = Console.ReadLine();
+            }
+                months.RemoveAt(7);
+                months.Insert(7,"___");
+                Console.WriteLine(string.Join(" ", months));
+                Console.WriteLine("Memorize your Verse!!!...Continue...?");
+                string userlastn = Console.ReadLine();
+             {
+                months.RemoveAt(4);
+                months.Insert(4,"___");
+                Console.WriteLine(string.Join(" ", months));
+                Console.WriteLine("Memorize your Verse!!!...Continue...?");
+                string userlast = Console.ReadLine();
+            }
+             {
+                months.RemoveAt(8);
+                months.Insert(8,"___");
+                Console.WriteLine(string.Join(" ", months));
+                Console.WriteLine("Memorize your Verse!!!...Continue...?");
+                string userlast = Console.ReadLine();
+            }
+             {
+                months.RemoveAt(3);
+                months.Insert(3,"___");
+                Console.WriteLine(string.Join(" ", months));
+                Console.WriteLine("Memorize your Verse!!!...Continue...?");
+                string userlast = Console.ReadLine();
+            }
+             {
+                months.RemoveAt(6);
+                months.Insert(6,"___");
+                Console.WriteLine(string.Join(" ", months));
+                Console.WriteLine("Memorize your Verse!!!...Continue...?");
+                string userlast = Console.ReadLine();
+            }
+             {
+                months.RemoveAt(10);
+                months.Insert(10,"___");
+                Console.WriteLine(string.Join(" ", months));
+                Console.WriteLine("Memorize your Verse!!!...Continue...?");
+                string userlast = Console.ReadLine();
+            }
+             {
+                months.RemoveAt(12);
+                months.Insert(12,"___");
+                Console.WriteLine(string.Join(" ", months));
+                Console.WriteLine("Memorize your Verse!!!...Continue...?");
+                string userlast = Console.ReadLine();
+            }
+             {
+                months.RemoveAt(17);
+                months.Insert(17,"___");
+                Console.WriteLine(string.Join(" ", months));
+                Console.WriteLine("Memorize your Verse!!!...Continue...?");
+                string userlast = Console.ReadLine();
+            }
+             {
+                months.RemoveAt(20);
+                months.Insert(20,"___");
+                Console.WriteLine(string.Join(" ", months));
+                Console.WriteLine("Memorize your Verse!!!...Continue...?");
+                string userlast = Console.ReadLine();
+            }
+             {
+                months.RemoveAt(25);
+                months.Insert(25,"___");
+                Console.WriteLine(string.Join(" ", months));
+                Console.WriteLine("Memorize your Verse!!!...Continue...?");
+                string userlast = Console.ReadLine();
+            }
+             {
+                months.RemoveAt(3);
+                months.Insert(23,"___");
+                Console.WriteLine(string.Join(" ", months));
+                Console.WriteLine("Memorize your Verse!!!...Continue...?");
+                string userlast = Console.ReadLine();
+            }
+             {
+                months.RemoveAt(21);
+                months.Insert(21,"___");
+                Console.WriteLine(string.Join(" ", months));
+                Console.WriteLine("Memorize your Verse!!!...Continue...?");
+                string userlast = Console.ReadLine();
+            }
+             {
+                months.RemoveAt(17);
+                months.Insert(17,"___");
+                Console.WriteLine(string.Join(" ", months));
+                Console.WriteLine("Memorize your Verse!!!...Continue...?");
+                string userlast = Console.ReadLine();
+            }
+             {
+                months.RemoveAt(15);
+                months.Insert(15,"___");
+                Console.WriteLine(string.Join(" ", months));
+                Console.WriteLine("Memorize your Verse!!!...Continue...?");
+                string userlast = Console.ReadLine();
+            }
+             {
+                months.RemoveAt(1);
+                months.Insert(1,"___");
+                Console.WriteLine(string.Join(" ", months));
+                Console.WriteLine("Memorize your Verse!!!...Continue...?");
+                string userlast = Console.ReadLine();
+            }
+             {
+                months.RemoveAt(0);
+                months.Insert(0,"___");
+                Console.WriteLine(string.Join(" ", months));
+                Console.WriteLine("Memorize your Verse!!!...Continue...?");
+                string userlast = Console.ReadLine();
+            }
+             {
+                months.RemoveAt(5);
+                months.Insert(5,"___");
+                Console.WriteLine(string.Join(" ", months));
+                Console.WriteLine("Memorize your Verse!!!...Continue...?");
+                string userlast = Console.ReadLine();
+            }
+             {
+                months.RemoveAt(8);
+                months.Insert(8,"___");
+                Console.WriteLine(string.Join(" ", months));
+                Console.WriteLine("Memorize your Verse!!!...Continue...?");
+                string userlast = Console.ReadLine();
+            }
+             {
+                months.RemoveAt(9);
+                months.Insert(9,"___");
+                Console.WriteLine(string.Join(" ", months));
+                Console.WriteLine("Memorize your Verse!!!...Continue...?");
+                string userlast = Console.ReadLine();
+            }
+             {
+                months.RemoveAt(4);
+                months.Insert(4,"___");
+                Console.WriteLine(string.Join(" ", months));
+                Console.WriteLine("Memorize your Verse!!!...Continue...?");
+                string userlast = Console.ReadLine();
+            }
+             {
+                months.RemoveAt(10);
+                months.Insert(10,"___");
+                Console.WriteLine(string.Join(" ", months));
+                Console.WriteLine("Memorize your Verse!!!...Continue...?");
+                string userlast = Console.ReadLine();
+            }
+             {
+                months.RemoveAt(12);
+                months.Insert(12,"___");
+                Console.WriteLine(string.Join(" ", months));
+                Console.WriteLine("Memorize your Verse!!!...Continue...?");
+                string userlast = Console.ReadLine();
+            }
+             {
+                months.RemoveAt(13);
+                months.Insert(13,"___");
+                Console.WriteLine(string.Join(" ", months));
+                Console.WriteLine("Memorize your Verse!!!...Continue...?");
+                string userlast = Console.ReadLine();
+            }
+             {
+                months.RemoveAt(18);
+                months.Insert(18,"___");
+                Console.WriteLine(string.Join(" ", months));
+                Console.WriteLine("Memorize your Verse!!!...Continue...?");
+                string userlast = Console.ReadLine();
+            }
+             {
+                months.RemoveAt(20);
+                months.Insert(20,"___");
+                Console.WriteLine(string.Join(" ", months));
+                Console.WriteLine("Memorize your Verse!!!...Continue...?");
+                string userlast = Console.ReadLine();
+            }
+             {
+                months.RemoveAt(22);
+                months.Insert(22,"___");
+                Console.WriteLine(string.Join(" ", months));
+                Console.WriteLine("Memorize your Verse!!!...Continue...?");
+                string userlast = Console.ReadLine();
+            }
+             {
+                months.RemoveAt(24);
+                months.Insert(24,"___");
+                Console.WriteLine(string.Join(" ", months));
+                Console.WriteLine("Memorize your Verse!!!...Continue...?");
+                string userlast = Console.ReadLine();
+            }
+             {
+                months.RemoveAt(14);
+                months.Insert(14,"___");
+                Console.WriteLine(string.Join(" ", months));
+                Console.WriteLine("GOOD JOB!!!!");
+                string userlast = Console.ReadLine();
+            }
+
+        }
     }
 }
